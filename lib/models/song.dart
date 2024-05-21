188ea8b323e9107cc4757f84344af9af57173e6d
OLADISEA <ladisea55@gmail.com>
2024-05-21 11:38:35 +0100

    Song model

---
+++ lib/models/song.dart
@@ -0,0 +1,7 @@
+class Song{
+  final String songName;
+  final String artistName;
+  final String albumArtImagePath;
+  final String audioPath;
+  Song({required this.songName, required this.artistName,required this.albumArtImagePath, required this.audioPath});
+}
