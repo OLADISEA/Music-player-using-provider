c96a0e604e4f1c3486cf1579b33e42805624980d
OLADISEA <ladisea55@gmail.com>
2024-05-22 09:48:24 +0100

    Added the textfield for the search music and also adjusted some design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,32 +27,74 @@ class _HomePageState extends State<HomePage> {
     playlistProvider.currentSongIndex =  songIndex;
     
     // NAVIGATE TO THE SONG PAGE
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => SongPage()));
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SongPage()));
 
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('P L A Y L I S T'),
+        title: const Center(child: Text('P L A Y L I S T')),
       ),
-      drawer: MyDrawer(),
-      body: Consumer<PlaylistProvider>(
-        builder: (context, value, child) {
-          final List<Song> playlist = value.playlist;
-            return ListView.builder(
-              itemCount: playlist.length,
-              itemBuilder: (context,index){
-                final Song song = playlist[index];
-              return ListTile(
-                title: Text(song.songName),
-                subtitle: Text(song.artistName),
-                leading: Image.asset(song.albumArtImagePath,width: 50,),
-                onTap: () => goToSong(index),
-              );
-              }
-            );
-        }
+      drawer: const MyDrawer(),
+
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 33,top: 20,bottom: 10,right: 73),
+            padding: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Theme.of(context).colorScheme.inversePrimary)
+            ),
+            child: const TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search in Library',
+                icon: Icon(Icons.search)
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Consumer<PlaylistProvider>(
+                builder: (context, value, child) {
+                  final List<Song> playlist = value.playlist;
+                  return ListView.builder(
+                      itemCount: playlist.length,
+                      itemBuilder: (context,index){
+                        final Song song = playlist[index];
+                        return Container(
+                          decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10)
+                              )
+                          ),
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: Text(song.songName),
+                                subtitle: Text(song.artistName),
+                                leading: Image.asset(song.albumArtImagePath,width: 50,),
+                                trailing: const Icon(Icons.more_vert),
+                                onTap: () => goToSong(index),
+                              ),
+                              Divider(
+                                indent: 17,
+                                color: Theme.of(context).colorScheme.primary,
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                  );
+                }
+            ),
+          ),
+        ],
+
       ),
     );
   }
