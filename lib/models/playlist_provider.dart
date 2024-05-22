47e9454d27734e3f1213681454fc5cab67ac748f
OLADISEA <ladisea55@gmail.com>
2024-05-22 09:49:38 +0100

    Implemented the functionalities for the loop mode so that at the end of the last song, the song can start from the first index again

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -2,163 +2,169 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:music_player/models/song.dart';
 
-class PlaylistProvider extends ChangeNotifier{
+class PlaylistProvider extends ChangeNotifier {
   final List<Song> _playlist = [
     // 1st song
     Song(
-        songName: 'ALL OF ME',
-        artistName: 'John Legend',
-        albumArtImagePath: 'assets/images/all_of_me.jpg',
-        audioPath: 'audio/all of me.mp3'
+      songName: 'ALL OF ME',
+      artistName: 'John Legend',
+      albumArtImagePath: 'assets/images/all_of_me.jpg',
+      audioPath: 'audio/all of me.mp3',
     ),
 
-    //2nd song
-
+    // 2nd song
     Song(
-        songName: 'Faded',
-        artistName: 'Alan Walker',
-        albumArtImagePath: 'assets/images/faded.jpg',
-        audioPath: 'audio/faded.mp3'
-    ),
-    Song(
-        songName: 'Faded',
-        artistName: 'Alan Walker',
-        albumArtImagePath: 'assets/images/faded.jpg',
-        audioPath: 'audio/faded.mp3'
+      songName: 'Faded',
+      artistName: 'Alan Walker',
+      albumArtImagePath: 'assets/images/faded.jpg',
+      audioPath: 'audio/faded.mp3',
     ),
   ];
 
-  //index of the current song playing
+  // Index of the current song playing
   int? _currentSongIndex;
 
-  /*
-  AUDIO PLAYER
-   */
-  //audio player
+  // Audio player
   final AudioPlayer _audioPlayer = AudioPlayer();
 
-  //duration
+  // Duration
   Duration _currentDuration = Duration.zero;
   Duration _totalDuration = Duration.zero;
-  //constructor
-  PlaylistProvider(){
-    listenToDuration();
-  }
-  //initially not playing
+
+  // Initially not playing
   bool _isPlaying = false;
 
-  //play the song
-  void play() async{
-    final String path = _playlist[currentSongIndex!].audioPath;
+  // Repeat mode
+  bool _isLoopMode = false;
+
+  // Constructor
+  PlaylistProvider() {
+    listenToDuration();
+  }
+
+  // Play the song
+  Future<void> play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
     await _audioPlayer.stop();
     await _audioPlayer.play(AssetSource(path));
     _isPlaying = true;
     notifyListeners();
   }
 
-  //pause the current song
-  void pause() async{
+  // Pause the current song
+  Future<void> pause() async {
     await _audioPlayer.pause();
     _isPlaying = false;
     notifyListeners();
-
   }
 
-
-
-  // resume playing
-  void resume() async{
+  // Resume playing
+  Future<void> resume() async {
     await _audioPlayer.resume();
     _isPlaying = true;
     notifyListeners();
   }
 
-  //pause or resume
-  void pauseOrResume() async{
-    if(_isPlaying){
-      pause();
-    }else{
-      resume();
+  // Pause or resume
+  Future<void> pauseOrResume() async {
+    if (_isPlaying) {
+      await pause();
+    } else {
+      await resume();
     }
     notifyListeners();
   }
 
-  //seek to a specific position in the current song
-  void seek(Duration position) async {
+  // Seek to a specific position in the current song
+  Future<void> seek(Duration position) async {
     await _audioPlayer.seek(position);
+    notifyListeners();
   }
 
-  //play the next song
-  void playNextSong(){
-    if(_currentSongIndex != null){
-      if(_currentSongIndex! < _playlist.length - 1){
-        //go to the next song if the song is not the last one
-        currentSongIndex = _currentSongIndex! +  1;
-
-      }else{
+  // Play the next song
+  Future<void> playNextSong() async {
+    if (_currentSongIndex != null) {
+      if (_currentSongIndex! < _playlist.length - 1) {
+        // Go to the next song if the current song is not the last one
+        currentSongIndex = _currentSongIndex! + 1;
+      } else if (_isLoopMode) {
+        // Loop back to the first song if repeat mode is enabled
         currentSongIndex = 0;
+      } else {
+        // Stop playback if there are no more songs
+        await _audioPlayer.stop();
+        _isPlaying = false;
+        notifyListeners();
       }
     }
   }
 
-  //play the previous song
-  void playPreviousSong() async{
-    // if more than two seconds have passed, restart the current song
-    if(_currentDuration.inSeconds > 2){
-
-    }
-    //if it is within the first two seconds of the song go to the previous song
-    else{
-      if(currentSongIndex! > 0){
-        currentSongIndex = currentSongIndex! - 1;
-      }else{
-        //if it is the first song, loop back to last song
+  // Play the previous song
+  Future<void> playPreviousSong() async {
+    if (_currentDuration.inSeconds > 2) {
+      await seek(Duration.zero);
+    } else {
+      if (_currentSongIndex! > 0) {
+        currentSongIndex = _currentSongIndex! - 1;
+      } else {
+        // If it is the first song, loop back to the last song
         currentSongIndex = _playlist.length - 1;
-
       }
     }
   }
 
-  //list to duration
-  void listenToDuration(){
-    //listen for total duration
+  // Listen to duration changes
+  void listenToDuration() {
+    // Listen for total duration
     _audioPlayer.onDurationChanged.listen((newDuration) {
       _totalDuration = newDuration;
       notifyListeners();
-
     });
-    //listen for current duration
+
+    // Listen for current duration
     _audioPlayer.onPositionChanged.listen((newPosition) {
       _currentDuration = newPosition;
       notifyListeners();
     });
 
-    //listen for song completion
+    // Listen for song completion
     _audioPlayer.onPlayerComplete.listen((event) {
       playNextSong();
     });
   }
 
-  //dispose audio player
-  /*
-  G E T T E R S
-   */
+  // Dispose audio player
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
+  // Getters
   List<Song> get playlist => _playlist;
   int? get currentSongIndex => _currentSongIndex;
   bool get isPlaying => _isPlaying;
   Duration get currentDuration => _currentDuration;
   Duration get totalDuration => _totalDuration;
+  bool get isLoopMode => _isLoopMode;
 
-  /*
-  S E T T E R S
-   */
-
-  set currentSongIndex(int? newIndex){
+  // Setters
+  set currentSongIndex(int? newIndex) {
     _currentSongIndex = newIndex;
-    if(newIndex != null){
+    if (newIndex != null) {
       play();
     }
     notifyListeners();
+  }
+
+  // Setters for repeat mode
+  set loopMode(bool isRepeatMode) {
+    _isLoopMode = isRepeatMode;
+    notifyListeners();
+  }
+
+  // Toggle repeat mode
+  void toggleLoopMode() {
+    loopMode = !_isLoopMode;
   }
 }
